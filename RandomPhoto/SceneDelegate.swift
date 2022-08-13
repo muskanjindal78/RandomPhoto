381c3e95392c6862be921f9e0b48191648f3df00
Muskan Jindal <muskan.jindal@dhani.com>
2022-08-13 18:29:54 +0530

    Random Photo Application

--- RandomPhoto/SceneDelegate.swift
+++ RandomPhoto/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  RandomPhoto
-//
-//  Created by Muskan Jindal on 13/08/22.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -1,19 +1,48 @@
-//
-//  ViewController.swift
-//  RandomPhoto
-//
-//  Created by Muskan Jindal on 13/08/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .white
+        return imageView
+    }()
 
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Random Photo", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        getRandomPhoto()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 30, y: view.frame.size.height - 100 - view.safeAreaInsets.bottom, width: view.frame.size.width - 70, height: 55)
+    }
+    
+    @objc func didTapButton() {
+        getRandomPhoto()
+    }
+    
+    func getRandomPhoto() {
+        let urlStr = "https://source.unsplash.com/random/300x300"
+        let url = URL(string: urlStr)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 }
 

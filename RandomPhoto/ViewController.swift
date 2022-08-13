75945ac8a97fe489e1caa44667c05c9486a899c1
Muskan Jindal <muskan.jindal@dhani.com>
2022-08-13 17:25:32 +0530

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Muskan Jindal on 13/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
